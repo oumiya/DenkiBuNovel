87a1f2c980b30d06c5f19d2609c60f6156f86143
oumiya <oumiya.s676@gmail.com>
2018-09-24 23:52:02 +0900

    プロジェクトの雛形を作成

---
+++ Assets/Scripts/ScenarioManager.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+[RequireComponent(typeof(TextController))]
+public class ScenarioManager : SingletonMonoBehaviourFast<ScenarioManager> {
+
+    public string LoadFileName;
+
+    private string[] m_scenarios;
+    private int m_currentLine = 0;
+    private bool m_isCallPreload = false;
+
+    private TextController m_textController;
+    private CommandController m_commandController;
+
+    void RequestNextLine()
+    {
+        var currentText = m_scenarios[m_currentLine];
+
+        m_textController.SetNextLine(CommandProcess(currentText));
+        m_currentLine++;
+        m_isCallPreload = false;
+    }
+
+    public void UpdateLines(string fileName)
+    {
+        string scenarioText = ReadFile(fileName);
+        if (scenarioText == null)
+        {
+            Debug.LogError("シナリオファイルが見つかりませんでした");
+            Debug.LogError("ScenarioManagerを無効化します");
+            enabled = false;
+            return;
+        }
+        m_scenarios = scenarioText.Split(new string[] { "@br" }, System.StringSplitOptions.None);
+        m_currentLine = 0;
+    }
+
+    /// <summary>
+    /// シナリオファイルの読み込み
+    /// </summary>
+    private string ReadFile(string fileName)
+    {
+        string scenarioText = string.Empty;
+
+        FileInfo fi = new FileInfo(Application.streamingAssetsPath + "/Scenario/" + fileName);
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8))
+            {
+                scenarioText = sr.ReadToEnd();
+            }
+        }
+        catch(System.Exception e)
+        {
+            scenarioText = null;
+        }
+
+        return scenarioText;
+    }
+
+    private string CommandProcess(string line)
+    {
+        var lineReader = new StringReader(line);
+        var lineBuilder = new StringBuilder();
+        var text = string.Empty;
+        while ((text = lineReader.ReadLine()) != null)
+        {
+            var commentCharacterCount = text.IndexOf("//");
+            if (commentCharacterCount != -1)
+            {
+                text = text.Substring(0, commentCharacterCount);
+            }
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (text[0] == '@' && m_commandController.LoadCommand(text))
+                    continue;
+                lineBuilder.AppendLine(text);
+            }
+        }
+
+        return lineBuilder.ToString();
+    }
+
+    #region UNITY_CALLBACK
+
+    void Start()
+    {
+        m_textController = GetComponent<TextController>();
+        m_commandController = GetComponent<CommandController>();
+
+        UpdateLines(LoadFileName);
+        RequestNextLine();
+    }
+
+    void Update()
+    {
+        if (m_textController.IsCompleteDisplayText)
+        {
+            if (m_currentLine < m_scenarios.Length)
+            {
+                if (!m_isCallPreload)
+                {
+                    m_commandController.PreloadCommand(m_scenarios[m_currentLine]);
+                    m_isCallPreload = true;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RequestNextLine();
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_textController.ForceCompleteDisplayText();
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+    /// <summary>
+    /// シナリオを格納する
+    /// </summary>
+    public string[] scenarios;
+    /// <summary>
+    /// uiTextへの参照を持つ
+    /// </summary>
+    [SerializeField]
+    public Text uiText;
+    /// <summary>
+    /// 1文字の表示にかかる時間
+    /// </summary>
+    [SerializeField][Range(0.001f, 0.3f)]
+    public float intervalForCharacterDisplay = 0.05f;
+    /// <summary>
+    /// 現在の文字列
+    /// </summary>
+    string currentText = string.Empty;
+    /// <summary>
+    /// 表示にかかる時間
+    /// </summary>
+    float timeUnitDisplay = 0;
+    /// <summary>
+    /// 文字列の表示を開始した時間
+    /// </summary>
+    float timeElapsed = 1;
+    /// <summary>
+    /// 表示中の文字数
+    /// </summary>
+    int lastUpdateCharacter = -1;
+    public bool IsCompleteDisplayText
+    {
+        get { return Time.time > timeElapsed + timeUnitDisplay; }
+    }
+    /// <summary>
+    /// 強制的に全文表示する
+    /// </summary>
+    public void ForceCompleteDisplayText()
+    {
+        timeUnitDisplay = 0;
+    }
+    /// <summary>
+    /// 次に表示する文字列をセットする
+    /// </summary>
+    public void SetNextLine(string text)
+    {
+        currentText = text;
+
+        // 想定時間と現在の時刻をキャッシュ
+        timeUnitDisplay = currentText.Length * intervalForCharacterDisplay;
+        timeElapsed = Time.time;
+
+        // 文字数カウンタを初期化
+        lastUpdateCharacter = -1;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // クリックから経過した時間が想定時間の何%かを確認し、表示文字数を出す
+        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUnitDisplay) * currentText.Length);
+
+        if (displayCharacterCount != lastUpdateCharacter)
+        {
+            uiText.text = currentText.Substring(0, displayCharacterCount);
+            lastUpdateCharacter = displayCharacterCount;
+        }
+	}
+}
